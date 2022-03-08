58c1bf0aa2ca6f8dc5e0038d7ff6cb9203c6af56
jaewookim <wodnqjel@gmail.com>
2022-03-08 22:17:38 +0900

    2022/03/08 수정작업

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+
+slidesPlugin();
+
+const toggleMenu = document.querySelector(".nav_hamburger");
+const toggleMenu_color = document.querySelector(".nav_hamburger_color");
+const menu = document.querySelector(".nav_menu");
+const header = document.querySelector("header")
+
+toggleMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  header.classList.toggle('active')
+})
+
+toggleMenu_color.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  header.classList.toggle('active')
+})
+
